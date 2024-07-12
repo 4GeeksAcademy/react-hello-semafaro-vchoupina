3a5d97e8dd20fe5fafbb75d197cbc124d16686d5
vchoupina <168114326+vchoupina@users.noreply.github.com>
2024-07-12 19:36:59 +0000

    semafaro

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const TrafficLight = () => {
+    const [selectedColor, setSelectedColor] = useState('none');
+    const [showPurple, setShowPurple] = useState(false);
+
+    const red = selectedColor === 'red' ? 'light red selected-red' : 'light red';
+    const yellow = selectedColor === 'yellow' ? 'light yellow selected-yellow' : 'light yellow';
+    const green = selectedColor === 'green' ? 'light green selected-green' : 'light green';
+    const purple = selectedColor === "purple" ? "light purple selected-purple" : "light purple";
+
+    const changeColor = (e) => {
+        setSelectedColor(e.target.id);
+        console.log(e.target.id);
+    }
+
+    const changeColorRandom = () => {
+        if (selectedColor === 'red') {
+            setSelectedColor('yellow');
+        } else if (selectedColor === 'yellow') {
+            setSelectedColor('green');
+        } else if (selectedColor === 'green') {
+            setSelectedColor('purple');
+        }
+        else if (selectedColor === 'purple') {
+            setSelectedColor('red');
+        }
+    }
+
+    const toggleShowPurple = () => {
+        setShowPurple(!showPurple);
+    };
+
+    const apagarLuces = () => {
+        if (selectedColor === 'none') {
+            setSelectedColor('red');
+        } else {
+            setSelectedColor('none');
+        }
+    };
+
+    return (
+        <>
+            <div className="container stick bg-dark"></div>
+            <div className="container traffic-light bg-dark">
+                <div onClick={changeColor} className={red} id="red"></div>
+                <div onClick={changeColor} className={yellow} id="yellow"></div>
+                <div onClick={changeColor} className={green} id="green"></div>
+                {showPurple && <div onClick={changeColor} className={purple} id="purple"></div>}
+            </div>
+
+            <div className="container-mando">
+                <div className="mando">
+                    <div className="top">
+                        <div className="marca"> TrafficLight </div>
+                        <button className="boton power" onClick={apagarLuces} title="Turn ON/OFF Lights">
+                            <i className="fa fa-power-off fa-2x"></i>
+                        </button>
+                    </div>
+
+                    <div className="container-buttons">
+                        <a onClick={changeColorRandom}><span></span><span></span><span></span><span></span>
+                            SWITCH LIGHTS
+                        </a>
+                        <a onClick={toggleShowPurple}><span></span><span></span><span></span><span></span>
+                            {showPurple ? "HIDE PURPLE" : "SHOW PURPLE"}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
-export default Home;
+export default TrafficLight;
